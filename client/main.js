fcadc4eece2bc3b4b86086bf02271da099bbcb22
WeiqiWang <ww15c@my.fsu.edu>
2018-11-16 10:58:44 -0800

    sync code

--- client/main.js
+++ client/main.js
@@ -3,6 +3,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+var subHandle;
+subHandle = Meteor.subscribe("_console");
+
+var sharedConsole = new Meteor.Collection('_clientConsole');
+// sharedConsole.find().observe({
+//   added: function(doc) {
+//     var args = ["Server (" + doc.createdAt.toUTCString() + "):"];
+//     for (var i = 0, ln = doc.args.length; i < ln; i++) {
+//         args.push(JSON.parse(doc.args[i]));
+//     }
+
+//     console.log.apply(console, args);
+//   }
+// });
+
 Template.realdemo.onCreated(function realdemoOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -12,11 +27,32 @@ Template.realdemo.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  // log(){
+  //   sharedConsole.find().observe({
+  //     added: function(doc) {
+  //       var args = ["Server (" + doc.createdAt.toUTCString() + "):"];
+  //       for (var i = 0, ln = doc.args.length; i < ln; i++) {
+  //           args.push(JSON.parse(doc.args[i]));
+  //       }
+  //       return args;
+  //       //console.log.apply(console, args);
+  //     }
+  //   });
+  // },
 });
 
 Template.realdemo.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    Meteor.call('runCode', function(e, r){
+        console.log('test');
+        if (!e){
+          console.log(e);
+        }
+        else
+        {
+          console.log(r);
+        }
+    });
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,84 @@
 import { Meteor } from 'meteor/meteor';
 
+const spawn = require('child_process').spawn;
+
+ConsoleMe = {};
+
 Meteor.startup(() => {
-  // code to run on server at startup
+  ConsoleMe.enabled = true;
+  
+  var sharedConsole = new Meteor.Collection('_console', {connection: null});
+  var originalLog = console.log;
+  console.log = Meteor.bindEnvironment(function() {
+    originalLog.apply(console, arguments);
+    if (ConsoleMe.enabled) {
+      // Stringify arguments, stopping recursion at any circular references
+      // to avoid errors.
+      var args = Array.prototype.slice.call(arguments);
+      var cache = [];
+      args = args.map(function (arg) {
+        return JSON.stringify(arg, function(key, value) {
+          if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+              // Circular reference found, discard key
+              return;
+            }
+            // Store value in our collection
+            cache.push(value);
+          }
+          return value;
+        });
+      });
+      cache = null; // Enable garbage collection
+      sharedConsole.insert({args: args, createdAt: new Date});
+    }
+  });
+
+  Meteor.publish("_console", function() {
+    var self = this;
+    var initializing = true;
+    var handle = sharedConsole.find().observe({
+      added: function(doc) {
+        if (!initializing) {
+          self.added("_clientConsole", doc._id, doc);
+          sharedConsole.remove({_id: doc._id});
+        }
+      }
+    });
+    initializing = false;
+    self.ready();
+
+    // Stop observing the cursor when client unsubs.
+    self.onStop(function() {
+      handle.stop();
+    });
+  });
 });
+
+Meteor.methods({
+  runCode: function () {
+    // This method call won't return immediately, it will wait for the
+    // asynchronous code to finish, so we call unblock to allow this client
+    // to queue other method calls (see Meteor docs)
+    /*this.unblock();
+    var future=new Future();
+    var path = process.env.PWD + "/contracts/micro_chain_test.js"
+    console.log(process.env.PWD);
+    var command="node " + path;
+    exec(command,function(error,stdout,stderr){
+      if(error){
+        console.log(error);
+        throw new Meteor.Error(500,command+" failed");
+      }
+      //future.return(stdout.toString());
+      console.log(stdout.toString());
+    });
+    return future.wait();*/
+    var path = process.env.PWD + "/contracts/micro_chain_test.js"
+    const ls = spawn("node",[path]);
+    ls.stdout.on('data', (data)=>{
+      console.log(`${data}`);
+    });
+  }
+});
+
